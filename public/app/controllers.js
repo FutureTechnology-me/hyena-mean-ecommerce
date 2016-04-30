64d1be31357edf0c7df899aff0b915f0b6157c22
jadebreed <jade.breed.mmp@gmail.com>
2016-04-30 23:46:28 +0000

    Comment cartController

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -96,9 +96,12 @@ $scope.$watch('shareService.carttax', function(){
 
 commerceApp.controller('cartController', ['$scope', '$location','$http','$log','$route', 'ngCart', 'shareService', function($scope, $location, $http, $log, $route, ngCart, shareService) {
 
+// get shipping total from shareService
 $scope.totalShipping = shareService.totalShipping;
 
+//watch shareService.totalShipping
 $scope.$watch('shareService.totalShipping', function(){
+    //Update $scope to match changes
     $scope.totalShipping = shareService.totalShipping;
 })
 
